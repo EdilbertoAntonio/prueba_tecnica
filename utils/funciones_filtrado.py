17c38ad57851f0364a6b7f1f07920d2bc0965d36
Edilberto Antonio Gómez García <edilbertoantoniogg@gmail.com>
2026-05-21 00:15:33 -0600

    entendimiento de las tablas y avance en limpieza de datos

--- utils/funciones_filtrado.py
+++ utils/funciones_filtrado.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def tipo_dato_x_columna(tabla: pd.DataFrame) -> pd.DataFrame:
     tipos = tabla.dtypes.reset_index()
-    tipos.columns = ['columna', 'tipo']
+    tipos.columns = ['columna', 'tipo de dato']
     return tipos
 
 def nulos_x_columna(tabla: pd.DataFrame) -> pd.DataFrame:
@@ -14,3 +14,12 @@ def unicos_x_columna(tabla: pd.DataFrame) -> pd.DataFrame:
     cantidad_nulos = tabla.nunique().reset_index()
     cantidad_nulos.columns = ['columna', 'cantidad de valores unicos']
     return cantidad_nulos
+
+def tipo_nulo_unicos_x_columna(tabla: pd.DataFrame) -> pd.DataFrame:
+    tipo = tipo_dato_x_columna(tabla)
+    nulos = nulos_x_columna(tabla)
+    unicos = unicos_x_columna(tabla)
+    datos = [df.set_index('columna') for df in [tipo, nulos, unicos]]
+
+    union = pd.concat(datos, axis=1, join='inner').reset_index()
+    return union
